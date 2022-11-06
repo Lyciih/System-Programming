ca44a2bce525060cedf1378a8d97245e0c17fb94
Lyciih <gnehciyil@gmail.com>
2022-11-06 19:50:25 +0800

    2

--- SIC2/assembler2.c
+++ SIC2/assembler2.c
@@ -20,12 +20,25 @@ int main(int argc, char *argv[])
         FILE *obj_file = fopen(obj_file_name,"w");
         printf("[%d] [target] %s\n\n", i, obj_file_name);
 
-        char buffer[8];
+        char buffer[50];
+        char temp1[50];
+        char temp2[50];
+        char temp3[50];
 
-        while (fgets(buffer, 8 , ass))
+
+
+        while(fgets(buffer, 50, ass))
         {
-            printf("%s %d", buffer, strlen(buffer));
+            memset(temp1, 0, 50);
+            memset(temp2, 0, 50);
+            memset(temp3, 0, 50);
+            
+
+            sscanf(buffer, "%s %s %s", temp1, temp2, temp3);
+            printf("%s %s %s\n", temp1, temp2, temp3);
+            printf("%d %d %d\n", strlen(temp1), strlen(temp2), strlen(temp3));
         }
+
         
 
 
